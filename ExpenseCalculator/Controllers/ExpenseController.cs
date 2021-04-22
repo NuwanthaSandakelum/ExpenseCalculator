54b9929605f73092069b4d6faec331b6dd92b76d
NuwanthaSandakelum <nuwantha101@gmail.com>
2021-04-22 16:11:51 +0530

    Add original project files to git

---
+++ ExpenseCalculator/Controllers/ExpenseController.cs
@@ -0,0 +1,113 @@
+﻿using ExpenseCalculator.Data;
+using ExpenseCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseCalculator.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> expenseObjList = _db.Expenses;
+            return View(expenseObjList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //GET-Delete
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id ==0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
+        }
+
+        //GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
+        }
+
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+
+        //POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+    }
+}
---
+++ ExpenseCalculator/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseCalculator.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public float Amount { get; set; }
+    }
+}
